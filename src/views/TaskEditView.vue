33eded6322230c324c4a015ded0e88776a0c9ed9
Mikael Virkkunen <mkael.virkkunen@gmail.com>
2024-06-07 23:05:43 +0300

    Small fixes

--- src/views/TaskEditView.vue
+++ src/views/TaskEditView.vue
@@ -1,43 +1,46 @@
 <template>
-    <v-form ref="form" v-model="valid" @submit.prevent="edit">
-        <v-card title="New Task">
-            <v-card-text>
-                <v-text-field
-                    v-model="task!.name"
-                    label="Task Name"
-                    :rules="[rules.required]"
-                    required
-                ></v-text-field>
-
-                <v-menu
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="290px"
-                >
-                    <template v-slot:activator="{ isActive, props }">
+    <div v-if="task == undefined"></div>
+    <div v-else>
+        <v-form ref="form" v-model="valid" @submit.prevent="edit">
+            <v-card title="New Task">
+                <v-card-text>
                     <v-text-field
-                        v-model="task!.dueDate"
-                        label="Due Date"
-                        prepend-icon="mdi-calendar"
-                        readonly
-                        v-bind="props"
-                        :v-on="isActive"
+                        v-model="task!.name"
+                        label="Task Name"
+                        :rules="[rules.required]"
+                        required
                     ></v-text-field>
-                    </template>
-                    <v-date-picker v-model="task!.dueDate" @input="menu = false"></v-date-picker>
-                </v-menu>
-            </v-card-text>
 
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn :disabled="!valid" color="success" @click="edit">
-                    Save
-                </v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-form>
+                    <v-menu
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                    >
+                        <template v-slot:activator="{ isActive, props }">
+                        <v-text-field
+                            v-model="task!.dueDate"
+                            label="Due Date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="props"
+                            :v-on="isActive"
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="task!.dueDate" @input="menu = false"></v-date-picker>
+                    </v-menu>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn :disabled="!valid" color="success" @click="edit">
+                        Save
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-form>
+    </div>
 </template>
 
 <script setup lang="ts">
--- src/views/TaskListView.vue
+++ src/views/TaskListView.vue
@@ -1,25 +1,42 @@
 <template>
     <div>
         <TaskCreateComponent></TaskCreateComponent>
-        <TaskListComponent 
-            v-for="task in store.tasks" 
-            :key="task.id"
-            :task="task">
-        </TaskListComponent>
+        <div v-if="loading">Loading tasks...</div>
+        <div v-else>
+            <TaskListComponent 
+                v-for="task in store.tasks" 
+                :key="task.id"
+                :task="task">
+            </TaskListComponent>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import TaskListComponent from '../components/TaskListComponent.vue';
 import TaskCreateComponent from '../components/TaskCreateComponent.vue';
 import { useTaskStore } from '../store/useTaskStore';
+import { watch, onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 
 const store = useTaskStore();
+const route = useRoute();
+const loading = ref(true);
 
 onMounted(async () => {
+    loading.value = true;
+    console.log('lol');
     await store.fetchTasks();
+    loading.value = false;
 });
+
+watch(route, async () => {
+    loading.value = true;
+    console.log('lol');
+    await store.fetchTasks();
+    loading.value = false;
+});
+
 
 </script>
 
